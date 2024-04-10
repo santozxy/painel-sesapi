fb1d9bf897cc188283547173f11f3f8bdab6a8f1
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-10 18:14:42 -0300

    Atualizar URL base para usar variável de ambiente

--- src/services/process/process.ts
+++ src/services/process/process.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 import { ProcessDTO } from "./processDTO";
 
+const base_url = import.meta.env.VITE_BASE_URL;
 async function searchProcess(process: string) {
   try {
-    const { data } = await axios.get<ProcessDTO>(
-      `https://api.painel.sei.pi.gov.br/api/processos/${process}/0`
-    );
+    const { data } = await axios.get<ProcessDTO>(`${base_url}/${process}/0`);
     return data;
   } catch (error) {
     console.log(error);
