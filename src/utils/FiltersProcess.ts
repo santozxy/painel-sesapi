897d6969bde437ebea15efd5a3bcf8dd4648c25e
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-11 17:59:05 -0300

    Corrigir lógica de filtragem de unidades no arquivo FiltersProcess.ts

--- src/utils/FiltersProcess.ts
+++ src/utils/FiltersProcess.ts
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { DetailResume, ProcessDTOResume, Unids } from "@services/process/processDTO";
+import {
+  DetailResume,
+  ProcessDTOResume,
+  Unids,
+} from "@services/process/processDTO";
 
 export function filterUnit(data: Unids, group: string, isMain: boolean) {
   const filteredUnids = Object.entries(data)
     .filter(([_, unidData]) => {
       const firstKey = Object.keys(unidData)[0];
       if (!isMain) {
-        return unidData[firstKey].group === group;
+        // return unidData[firstKey].group === group;
       }
-      return unidData[firstKey].group !== group;
+      // return unidData[firstKey].group !== group;
     })
     .reduce((acc, [key, value]) => {
       acc[key] = value;
@@ -17,13 +21,14 @@ export function filterUnit(data: Unids, group: string, isMain: boolean) {
   return filteredUnids;
 }
 
-
 export function dataResumeObj(response: ProcessDTOResume) {
   const dataResume: DetailResume[] = [];
   Object.entries(response).forEach(([key, value]) => {
     Object.entries(value).forEach(([subkey, detail]) => {
-      Object.entries(detail).forEach(([subsubkey, subdetail]) => {
+      const detailResume = detail as DetailResume;
+      Object.entries(detailResume).forEach(([subsubkey, subdetail]) => {
         dataResume.push(subdetail as DetailResume);
+        console.log(subdetail as DetailResume)
       });
     });
   });
