23770a1abaf43334183da5ac31699d45dacd9094
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-11 15:59:35 -0300

    Atualizar interface ProcessDTO e ProcessDTOResume para adicionar propriedades e detalhes

--- src/services/process/processDTO.ts
+++ src/services/process/processDTO.ts
@@ -1,3 +1,4 @@
+//// ProcessDTO detalhado com passar o parametro 0 na rota
 export interface Unids {
   [key: string]: Detail;
 }
@@ -21,4 +22,53 @@ export interface Detail {
   end: string;
   minutes: number;
   seconds: number;
+}
+
+
+
+export interface ProcessDTO {
+  protocolo: string;
+  typeDescription: string;
+  startDate: string;
+  endDate: string;
+  duration: string;
+  days: number;
+  hours: number;
+  type: string;
+  unids: Unids ;
+}
+
+
+
+//// ProcessDTO resumido com passar o parametro 1 na rota
+
+export interface ProcessDTOResume {
+  protocolo: string;
+  typeDescription: string;
+  startDate: string;
+  endDate: string;
+  duration: string;
+  days: number;
+  hours: number;
+  type: string;
+  unids: Unids ;
+}
+export interface UnidsResume {
+  [unitId: string]: UnitResume;
+}
+
+export interface UnitResume {
+  [order: string]: DetailResume;
+}
+
+export interface DetailResume {
+  seconds: number;
+  minutes: number;
+  unidDescription: string;
+  typeGroup: string;
+  group: string;
+  start: string;
+  end: string;
+  hours: number;
+  days: number;
 }
--- src/services/process/processService.ts
+++ src/services/process/processService.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { ProcessDTO } from "./processDTO";
+import { ProcessDTO, ProcessDTOResume } from "./processDTO";
 
 const base_url = import.meta.env.VITE_BASE_URL;
-async function searchProcess(process: string) {
+async function searchProcess(process: string, mode: number) {
   try {
-    const { data } = await axios.get<ProcessDTO>(`${base_url}/${process}/0`);
+    const { data } = await axios.get<ProcessDTO | ProcessDTOResume>(`${base_url}/${process}/${mode}`);
     return data;
   } catch (error) {
     console.log(error);
