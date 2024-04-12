52c2c0ce37d88e122d7e98a0b93555ad483ba914
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-12 16:02:52 -0300

    Corrigir bug na função App que limpava os dados antes de atualizá-los

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ function App() {
             draggable: true,
             position: "top-right",
           });
+          setData(undefined);
           setDataGrouped([]);
           setDataDetailed([]);
         }
