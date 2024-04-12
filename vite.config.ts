8272ffc8a9f5d83ce31d7eb34368f747e75448f5
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-12 09:12:31 -0300

    Comentário

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build:{
-    assetsDir: "painel/assets",
-  },
+  // build:{
+  //   assetsDir: "painel/assets",
+  // },
   plugins: [react()],
   resolve: {
     alias: {
