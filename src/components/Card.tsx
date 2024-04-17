b8fbd6ad9bae5debaf553c126d3768a9d7b49a62
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-17 17:00:38 -0300

    :refactor: Retirando imports desnecessários

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import React from "react";
 import { Detail } from "@services/process/processDTO";
 import { ChevronDownIcon, ChevronUpIcon, LogIn, LogOut } from "lucide-react";
