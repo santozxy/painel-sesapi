3050314e38939d2c8764dbc0c3d3317385dc69bb
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-17 13:31:18 -0300

    :lipstick: Ajustando responsividade

--- src/components/BoxDurationProcess.tsx
+++ src/components/BoxDurationProcess.tsx
@@ -10,19 +10,20 @@ export function BoxDurationProcess({ duration = "", process = "" }) {
   }
   return (
     <Draggable
+      defaultClassName="max-sm:w-full"
       bounds="html"
       onStart={() => setIsDragging(true)}
       onStop={() => setIsDragging(false)}
     >
       {duration ? (
         <div
-          className={`fixed bottom-4 right-4 bg-white max-sm:bottom-0 max-sm:right-0 border border-gray-300 rounded-lg shadow-md text-lg cursor-grab z-50 ${
+          className={`fixed bottom-4 right-4 bg-white max-sm:bottom-0 max-sm:right-0 max-sm:left-0 border border-gray-300 rounded-lg shadow-md text-lg cursor-grab z-50 ${
             isDragging ? "shadow-primary" : ""
           }`}
         >
           <div className="bg-primary flex gap-3 items-center justify-between p-2 border-b-2 rounded-t-md">
             <Timer size={22} color="#f5f5f5" />
-            <h2 className="font-semibold text-center max-sm:text-small text-terciary-light">
+            <h2 className="font-semibold text-center max-sm:text-sm text-terciary-light">
               {process}
             </h2>
             <button
@@ -34,7 +35,7 @@ export function BoxDurationProcess({ duration = "", process = "" }) {
             </button>
           </div>
 
-          <h2 className="font-medium p-2 max-sm:text-small">{duration}</h2>
+          <h2 className="font-medium p-2 max-sm:text-sm">{duration}</h2>
         </div>
       ) : (
         <></>
