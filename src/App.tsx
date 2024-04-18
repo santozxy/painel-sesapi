d32ce5a02dc8a13f461f8d6b10479249cf77fc3c
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-18 16:29:40 -0300

    :sparkles: Add requisições com reactQuery :recycle: removendo grandes linhas de código :zap: Melhorando desempenho de persistência de dados e requisições

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,59 @@
 import "react-toastify/dist/ReactToastify.css";
-import { useCallback, useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import { isAxiosError } from "axios";
 import { Header } from "./components/Header";
 import { SearchBar } from "./components/SearchBar";
-import { Detail, ProcessData } from "./services/process/processDTO";
+import { Detail } from "./services/process/processDTO";
 import { searchProcess } from "./services/process/processService";
 import { Loading } from "@components/Loading";
 import { getDataDetailed, getDataGrouped } from "@utils/filtersProcess";
 import { ListCards } from "@components/ListCards";
 import { BoxDurationProcess } from "@components/BoxDurationProcess";
 import { TableProcess } from "@components/TableProcess";
+import { useQuery } from "@tanstack/react-query";
 function App() {
   const [process, setProcess] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState<ProcessData>();
+
   const [dataGrouped, setDataGrouped] = useState<Detail[]>([]);
   const [dataDetailed, setDataDetailed] = useState<Detail[]>([]);
   const [dataFilterOthers, setDataFilterOthers] = useState<Detail[]>([]);
 
-  const fetchData = useCallback(async () => {
-    setLoading(true);
-    const processStr = process.replace(/[^\w\s]/g, "");
-    try {
-      const response = await searchProcess(processStr);
-      if (response) {
-        const dataGrouped = getDataGrouped(response.grouped);
-        const dataDetailed = getDataDetailed(response.detailed);
-        const filteredGroup = dataGrouped.filter(
-          (data) => data.group !== "OUTROS"
-        );
-        const filteredOthers = dataGrouped.filter(
-          (data) => data.group === "OUTROS"
-        );
-        setDataGrouped(filteredGroup);
-        setDataFilterOthers(filteredOthers);
-        setDataDetailed(dataDetailed);
-        setData(response);
-        setProcess("");
-        window.scrollTo({
-          top: 150,
-          behavior: "smooth",
-        });
-      }
-      setLoading(false);
-    } catch (error) {
-      setData(undefined);
+  const { isLoading, isError, data, error } = useQuery({
+    queryKey: ["searchProcess", process],
+    queryFn: () => searchProcess(process.replace(/[^\w\s]/g, "")),
+    enabled: process.length === 20,
+    initialData: undefined,
+    retry: false,
+    refetchOnWindowFocus: false,
+    refetchInterval: false,
+  });
+
+  if (isError) {
+    if (isAxiosError(error)) {
+      toast.error(error.response?.data?.message || "Erro ao buscar processo");
+    }
+  }
+
+  useMemo(() => {
+    if (data) {
+      const dataGrouped = getDataGrouped(data.grouped);
+      const dataDetailed = getDataDetailed(data.detailed);
+      const filteredGroup = dataGrouped.filter(
+        (data) => data.group !== "OUTROS"
+      );
+      const filteredOthers = dataGrouped.filter(
+        (data) => data.group === "OUTROS"
+      );
+      setDataGrouped(filteredGroup);
+      setDataFilterOthers(filteredOthers);
+      setDataDetailed(dataDetailed);
+    } else {
       setDataGrouped([]);
       setDataFilterOthers([]);
       setDataDetailed([]);
-      setProcess("");
-      setLoading(false);
-      if (isAxiosError(error)) {
-        toast.error(error.response?.data?.message || "Erro ao buscar processo");
-      } else {
-        console.error(error);
-      }
     }
-  }, [process]);
-
-  useEffect(() => {
-    if (process.length === 20) {
-      fetchData();
-    }
-  }, [fetchData, process]);
+  }, [data]);
 
   return (
     <div className="flex-col flex gap-10 pb-20">
@@ -71,9 +61,9 @@ function App() {
       <ToastContainer pauseOnHover={false} />
       <BoxDurationProcess duration={data?.duration} />
       <div className="flex gap-5 flex-wrap-reverse justify-center items-center mt-20 max-sm:mt-60">
-        <SearchBar loading={loading} setProcess={setProcess} />
+        <SearchBar loading={isLoading} setProcess={setProcess} />
       </div>
-      {loading ? (
+      {isLoading ? (
         <Loading />
       ) : (
         <div className="flex flex-col justify-center items-center mt-5">
