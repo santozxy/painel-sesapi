156eda3c95e836e085925a86bec9f3b80433ab11
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-19 10:59:32 -0300

    :recycle: Importação dinamica

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+import { BoxDurationProcess } from "./BoxDurationProcess";
+import { Header } from "./Header";
+import { ListCards } from "./ListCards";
+import { Loading } from "./Loading";
+import { SearchBar } from "./SearchBar";
+import { TableProcess } from "./TableProcess";
+
+export {
+  BoxDurationProcess,
+  Header,
+  ListCards,
+  Loading,
+  SearchBar,
+  TableProcess,
+};
