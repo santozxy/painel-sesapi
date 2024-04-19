43c0d0c01d76c935f37fb9e3661d9c46129ec12f
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-19 11:39:48 -0300

    Adicionar dependências @tanstack/react-router e @tanstack/router-vite-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/painel",
-  plugins: [react()],
+  plugins: [react(), TanStackRouterVite()],
   resolve: {
     alias: {
+      "@pages": "/src/pages",
       "@components": "/src/components",
       "@utils": "/src/utils",
       "@services": "/src/services",
