52d2b00ddc5bfbed5d8144494f264cd7530cbb65
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-04-20 22:48:05 -0300

    :lipstick: Adicionar cores de tema claro e tema escuro no tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ export default {
   theme: {
     extend: {
       colors: {
+        light: "#f5f5f5",
+        dark: "#2f2f2f",
         primary: "#107BB2",
         background: "#f5f5f5",
         placeholder: "#a0a0a0",
