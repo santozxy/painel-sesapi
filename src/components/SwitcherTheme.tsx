c209b09dd89646a0ad3c2bdfd655f1764824f538
Monnuery Júnior <monnuerypachecodossantos@gmail.com>
2024-04-20 22:47:25 -0300

    :sparkles: Adicionar um toggle  de alternar entre tema claro e tema escuro

--- src/components/SwitcherTheme.tsx
+++ src/components/SwitcherTheme.tsx
@@ -1,32 +1,18 @@
-// src/components/ThemeSwitcher.js
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Moon, Sun } from "lucide-react";
+import useTheme from "../hooks/useTheme";
 
 export function ThemeSwitcher() {
-  const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const isDarkMode = localStorage.getItem("darkMode") === "true";
-    setDarkMode(isDarkMode);
-  }, []);
-
-  useEffect(() => {
-    document.documentElement.classList.toggle("dark", darkMode);
-    localStorage.setItem("darkMode", darkMode.toString()); // Convert darkMode to string
-  }, [darkMode]);
-
-  const toggleDarkMode = () => {
-    setDarkMode((prevMode) => !prevMode);
-  };
+  const { colorTheme, toggleTheme } = useTheme();
 
   return (
     <button
-      onClick={toggleDarkMode}
+      onClick={toggleTheme}
       className={`fixed bottom-6 left-4 w-14 h-14 rounded-full flex items-center justify-center z-50 ${
-        darkMode ? "bg-light" : "bg-dark"
+        colorTheme === "dark" ? "bg-light" : "bg-dark"
       } transition-all duration-500 ease-in-out`}
     >
-      {darkMode ? (
+      {colorTheme === "dark" ? (
         <Moon size={24} className="text-dark" />
       ) : (
         <Sun size={24} className="text-light" />
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "@utils";
+import { ThemeSwitcher } from "./components/SwitcherTheme.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <ThemeSwitcher />
       <App />
     </QueryClientProvider>
   </React.StrictMode>
