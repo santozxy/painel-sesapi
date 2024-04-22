fec88f9d63ccba77b2941c7b504047c76ebd845f
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-22 17:34:03 -0300

    :lipstick: Ajustar layout e adicionar transição de cores no tema

--- src/components/SwitcherTheme.tsx
+++ src/components/SwitcherTheme.tsx
@@ -8,7 +8,7 @@ export function ThemeSwitcher() {
   return (
     <button
       onClick={toggleTheme}
-      className={`fixed bottom-6 sm:left-4 w-14 h-14 shadow-md  rounded-full flex items-center justify-center z-[99] max-sm:bottom-28 max-sm:right-4 max-sm:w-12 max-sm:h-12 border-2 ${
+      className={`fixed bottom-6 w-14 h-14 border-2 rounded-full flex items-center justify-center z-[99] shadow-md sm:left-4 max-sm:bottom-28 max-sm:right-4 max-sm:w-12 max-sm:h-12  ${
         colorTheme === "dark" ? "bg-light" : "bg-dark"
       } transition-all duration-500 ease-in-out`}
     >
