f67ee9d9bd6c86e0bad1f2d4c2600607a1415ee5
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-22 17:33:43 -0300

    :broom: Ajustar layout e remover funções desnecessárias

--- src/components/BoxDurationProcess.tsx
+++ src/components/BoxDurationProcess.tsx
@@ -3,7 +3,6 @@ import React, { useState } from "react";
 import Draggable from "react-draggable";
 
 export default function BoxDurationProcess({ duration = "" }) {
-  const [isDragging, setIsDragging] = useState(false);
   const [toggleTypeDuration, setToggleTypeDuration] = useState(1);
 
   const handleToggleTime = (id: number) => {
@@ -12,21 +11,17 @@ export default function BoxDurationProcess({ duration = "" }) {
 
   return (
     <Draggable
-      defaultClassName="border dark:border-gray-600 rounded-md"
+      defaultClassName="border dark:border-gray-600 rounded-md cursor-grab"
       bounds="html"
-      onStart={() => setIsDragging(true)}
-      onStop={() => setIsDragging(false)}
     >
       {duration ? (
         <div
-          className={`fixed bottom-4 right-4  bg-white max-sm:bottom-5 max-sm:right-2 max-sm:left-2 shadow-md text-lg z-50 ${
-            isDragging ? "shadow-primary" : ""
-          }`}
+          className={`fixed bottom-4 right-1/3 left-1/3  mx-auto shadow-md text-lg z-50 bg-white max-sm:bottom-5 max-sm:right-2 max-sm:left-2`}
         >
           <div className="flex justify-between">
             <button
               onClick={() => handleToggleTime(1)}
-              className={`bg-primary dark:bg-dark ${toggleTypeDuration === 1 ? "bg-opacity-100":"bg-opacity-60"} w-full flex  gap-3 items-center justify-center p-2 border-b-2 rounded-t-md cursor-pointer`}
+              className={`bg-primary dark:bg-dark ${toggleTypeDuration === 1 ? "bg-opacity-100" : "bg-opacity-60"} w-full flex  gap-3 items-center justify-center p-2 border-b-2 rounded-t-md `}
             >
               <Timer size={20} className="text-white" />
               <h2 className="font-semibold text-center max-sm:text-sm text-light">
@@ -44,7 +39,9 @@ export default function BoxDurationProcess({ duration = "" }) {
           </div>
 
           <h2 className="font-medium p-2 max-sm:text-sm">
-            {toggleTypeDuration === 1 ? duration : "24 dia(s) 03 hora(s) 20 minuto(s) 8 segundo(s)"}
+            {toggleTypeDuration === 1
+              ? duration
+              : "24 dia(s) 03 hora(s) 20 minuto(s) 8 segundo(s)"}
           </h2>
         </div>
       ) : (
