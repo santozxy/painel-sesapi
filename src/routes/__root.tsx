4db32d49dd3adb2e3ff735b5487e804b4204a5d9
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-04-23 15:55:01 -0300

    :wastebasket: Removendo arquivos não usados...

--- src/routes/__root.tsx
+++
@@ -1,7 +0,0 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { NotFound } from "@pages";
-
-export const Route = createRootRoute({
-  component: () => <Outlet />,
-  notFoundComponent: () => <NotFound />,
-});
--- src/routes/painel/index.tsx
+++
@@ -1,6 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Painel } from "@pages";
-
-export const Route = createFileRoute("/painel/")({
-  component: () => <Painel />,
-});
