194750c2b8aa0cb3ad834933a31cde2cc81cab4d
Monnuery Junior <monnuerypachecodossantos@gmail.com>
2024-07-24 16:05:21 -0300

    UEP-42: :truck: move/rename: Configurando estrutura de pastas

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
 import { App } from "./App";
 
-const router = createRouter({
-  routeTree,
-});
-
-declare module "@tanstack/react-router" {
-  interface Register {
-    router: typeof router;
-  }
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
